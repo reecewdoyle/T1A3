41e5fed9bbce28b54e78157053abbafe40c59d16
reecewdoyle <reece.doyle@icloud.com>
2023-12-13 22:16:57 +1100

    added more detail to pseudocode

--- lmpl.py
+++ lmpl.py
@@ -2,7 +2,31 @@
 
 # Welcome to the Lake Macquarie Premier League App
 
-# 1. Print fixture for this round
+# 1. Print fixture this round.
+            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
+            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
+            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
+            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
+# 2. Print fixture for the following rounds:
+    # 1. Round 1
+            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
+            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
+            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
+            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
+    # 2. Round 2
+    # 3. Round 3
+    # 4. Round 4
+    # 5. Round 5       
+    # 6. Round 6
+    # 7. Round 7
+    # 8. Round 8
+    # 9. Round 9
+    # 10. Round 10
+    # 11. Round 11
+    # 12. Round 12
+    # 13. Round 13
+    # 14. Round 14
+
 # 2. Print fixture for a team
     # 1. Belmont Bandits
     # 2. Boolaroo Bulldogs
