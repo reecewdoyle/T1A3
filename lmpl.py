b47d3f9397ac4cef78dd3205258e990002866027
reecewdoyle <reece.doyle@icloud.com>
2023-12-19 08:52:46 +1100

    fixed feature 4

--- lmpl.py
+++ lmpl.py
@@ -1,7 +1,7 @@
 from lmpl_functions import print_teams, print_this_round, print_round, team_draw, game_results
 
 # App Greeting
-print("Welcome to the Lake Macquarie Premier League App for 23/24!")
+print("\n""Welcome to the Lake Macquarie Premier League App for 23/24!""\n")
 
 
 def create_menu():
--- lmpl_functions.py
+++ lmpl_functions.py
@@ -1,5 +1,10 @@
+import csv
+
+
+
 def print_teams():
     teams = ["\n" "Belmont Bandits", "Boolaroo Bulldogs", "Charlestown Cobras", "Eleebana Eagles", "Glendale Guardians", "Speers Point Spartans", "Swansea Silverbacks", "Warners Bay Wanderers" "\n"]
+    print("\n""Congratulations to the following teams who've won selection in the Lake Macquiare Premier Leauge for the 23/24 season: ")
     print(*teams, sep="\n")
 # Print out the list of teams particiapting in the competition this year.
 
@@ -11,13 +16,45 @@ def print_this_round():
 # Print only the games for the round this week
     
 def print_round():
-    print("Print one of the rounds")
+
+    with open("lmpl.csv", "r") as f:
+        reader = csv.reader(f)
+        round = input("Enter the round (1-14) you'd like to print: ""\n")
+        for row in reader:
+            if row[1] == round:    
+                print("Round " + row[1] + ","" " +row[2] + " " + "vs" + " " + row[3])
+                print("\n")
 # Enter the round (1-14) you'd like to see the results or fixtures for.
 # Print the results and fixtures. 
 
 
+
 def team_draw():
-    print("Team draw view")
+    print("1. Belmont Bandits")
+    print("2. Boolaroo Bulldogs")
+    print("3. Charlestown Cobras")
+    print("4. Eleebana Eagles")
+    print("5. Glendale Guardians")
+    print("6. Speers Point Spartans")
+    print("7. Swansea Silverbacks")
+    print("8. Warners Bay Wanderers")
+
+    with open("lmpl.csv", "r") as f:
+        reader = csv.reader(f)
+        team = int(input("Team Numner: "))
+        teams_menu= ["Belmont Bandits", "Boolaroo Bulldogs", "Charlestown Cobras", "Eleebana Eagles", "Glendale Guardians", "Speers Point Spartans", "Swansea Silverbacks", "Warners Bay Wanderers"]
+        team_draw = teams_menu[((team) - 1)]
+        for row in reader:
+                if row[2] == team_draw:    
+                    print("Round " + row[1] + ","" " +row[2] + " " + "vs" + " " + row[3])
+                elif row[3] == team_draw:
+                    print("Round " + row[1] + ","" " +row[2] + " " + "vs" + " " + row[3])
+
+        # else:
+        #     print("Invalid Input")
+
+
+    # print("Team draw view")
 # Ask for name of the team
 # Print out only the row that features a match with the team name. Should always be 14 games total.
 # Game data should include Round, Home Team, Away Team, Game Number, Field, Time, Date, Day of the week.
@@ -27,3 +64,34 @@ def game_results():
 # Enter game number.
 # Enter Home Team
 # Enter Away Goals
+
+
+# def select_team_draw():
+#     print("Enter the number that corresponds to the team you'd like to see the season draw for.")
+#     print("1. Belmont Bandits")
+#     print("2. Boolaroo Bulldogs")
+#     print("3. Charlestown Cobras")
+#     print("4. Eleebana Eagles")
+#     print("5. Glendale Guardians")
+#     print("6. Speers Point Spartans")
+#     print("7. Swansea Silverbacks")
+#     print("8. Warners Bay Wanderers")
+
+
+        # team_menu = int(input("Enter the number that corresponds to the team to see their draw: ""\n"))
+        # if team_menu == "1":
+        #     team_draw = input("Belmont Bandits")
+        # elif team_menu == "2":
+        #     team_draw = input("Boolaroo Bulldogs")
+        # elif team_menu == "3":
+        #     team_draw = input("Charlestown Cobras")
+        # elif team_menu == "4":
+        #     team_draw = input("Charlestown Cobras")
+        # elif team_menu == "5":
+        #     team_draw = input("Charlestown Cobras")
+        # elif team_menu == "6":
+        #     team_draw = input("Charlestown Cobras")
+        # elif team_menu == "7":
+        #     team_draw = input("Charlestown Cobras")
+        # elif team_menu == "8":
+        #     team_draw = input("Charlestown Cobras")    
