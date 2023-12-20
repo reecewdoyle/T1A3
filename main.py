dbd17d48f91b4684949e861aa0c1fbb960d2389a
reecewdoyle <reece.doyle@icloud.com>
2023-12-20 17:44:38 +1100

    tweaked position of text on main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import csv
 
 text = pyfiglet.print_figlet(text = "Lake Macquarie Premier League", font = "standard", colors = "red")
 
-print(f"{fore('red')} {back('white')} Welcome to the Lake Macquarie Premier League App for 23/24!{attr('reset')}")
+print(f"{fore('red')}{back('white')} Welcome to the Lake Macquarie Premier League App for 23/24!{attr('reset')}\n")
 
 file_name = "results.csv"
 
@@ -15,7 +15,7 @@ try:
     # open the file in read mode
     results = open(file_name, "r")
     results.close()
-    print("In try block")
+    # print("In try block")
     # if it throws an error, the file doesn't exist
     # if no error, the files exists
 except FileNotFoundError:
@@ -24,19 +24,19 @@ except FileNotFoundError:
     results = open(file_name, "w")
     results.write("Game Number,Home Team,Home Goals,Away Team,Away Goals\n")
     results.close()
-    print("In except block")
+    # print("In except block")
 
 def create_menu():
-    print(f"{fore('blue')}Enter 1 to view THIS YEAR'S TEAMS. {attr('reset')}")
-    print(f"{fore('blue')}Enter 2 to view THIS WEEK'S GAMES. {attr('reset')}")
-    print(f"{fore('blue')}Enter 3 to view a ROUND. {attr('reset')}")
-    print(f"{fore('blue')}Enter 4 to view a TEAM'S DRAW. {attr('reset')}")
-    print(f"{fore('blue')}Enter 5 to ENTER the RESULTS from a game.  {attr('reset')}")
-    print(f"{fore('blue')}Enter 6 to REMOVE the RESULTS from a game.  {attr('reset')}")
-    print(f"{fore('blue')}Enter 7 to VIEW the RESULTS.  {attr('reset')}")
-    print(f"{fore('blue')}Enter 8 to exit.")
+    print(f"{fore('blue')} Enter 1 to view THIS YEAR'S TEAMS. {attr('reset')}")
+    print(f"{fore('blue')} Enter 2 to view THIS WEEK'S GAMES. {attr('reset')}")
+    print(f"{fore('blue')} Enter 3 to view a ROUND. {attr('reset')}")
+    print(f"{fore('blue')} Enter 4 to view a TEAM'S DRAW. {attr('reset')}")
+    print(f"{fore('blue')} Enter 5 to ENTER the RESULTS from a game.  {attr('reset')}")
+    print(f"{fore('blue')} Enter 6 to REMOVE the RESULTS from a game.  {attr('reset')}")
+    print(f"{fore('blue')} Enter 7 to VIEW the RESULTS.  {attr('reset')}")
+    print(f"{fore('blue')} Enter 8 to exit.")
     print("\n")
-    choice = input(f"{fore('red')} {back('white')}Enter your selection:{attr('reset')}")
+    choice = input(f"{fore('red')} {back('white')} Enter your selection:{attr('reset')}")
     return choice
 
 users_choice = ""
