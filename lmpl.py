157f22d9205e989fc5ec311053e57191fec78487
reecewdoyle <reece.doyle@icloud.com>
2023-12-20 11:15:36 +1100

    added completed boolean to fix loop issue in fucntion 4

--- lmpl.py
+++ lmpl.py
@@ -4,12 +4,11 @@ import pyfiglet
 
 # App Greeting
 
-
-# logo = pyfiglet.figlet_format(text = "Lake Macquarie Premier League", font = "standard", color = "red")
 text = pyfiglet.print_figlet(text = "Lake Macquarie Premier League", font = "standard", colors = "red")
-# print(logo)
 
 print(f"{fore('red')} {back('white')} Welcome to the Lake Macquarie Premier League App for 23/24!{attr('reset')}")
+
+
 
 def create_menu():
     print(f"{fore('blue')}Enter 1 to view THIS YEAR'S TEAMS. {attr('reset')}")
@@ -45,42 +44,6 @@ while users_choice != "6":
 print(f"{fore('red')} {back('white')} Thank you for using the Lake Macquarie Premier League App!{attr('reset')}")
 
 
-# 1. Print fixture this round.
-            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
-            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
-            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
-            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
-# 2. Print fixture for the following rounds:
-    # 1. Round 1
-            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
-            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
-            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
-            # Game number, Home Team, Away Team, Oval, Time, Day, Date,
-    # 2. Round 2
-    # 3. Round 3
-    # 4. Round 4
-    # 5. Round 5       
-    # 6. Round 6
-    # 7. Round 7
-    # 8. Round 8
-    # 9. Round 9
-    # 10. Round 10
-    # 11. Round 11
-    # 12. Round 12
-    # 13. Round 13
-    # 14. Round 14
-
-# 3. Print fixture for a team
-    # 1. Belmont Bandits
-    # 2. Boolaroo Bulldogs
-    # 3. Charlestown Cobras
-    # 4. Eleebana Eagles
-    # 5. Glendale Guardians
-    # 6. Speers Point Spartans
-    # 7. Swansea Silverbacks
-    # 8. Warners Bay Wanderers
-
-# 4. Print League Table
 
 # 5. Enter Results
     # 1. Game number
