3d6448c5f975550be119276ac9528e7b61100a0b
reecewdoyle <reece.doyle@icloud.com>
2023-12-23 21:33:17 +1100

    cleaned up some code that I'd commented out after improving

--- lmpl_functions.py
+++ lmpl_functions.py
@@ -70,30 +70,6 @@ def print_this_round():
 
     if not found_games and week_num in range(6, 43):
         print("No Games this week.")
-    
-# def print_this_round():
-#     from datetime import date
-#     # Shows the actual date
-#     my_date = datetime.date.today()
-#     print(my_date)
-#     # Using isocalendar() function
-#     year, week_num, day_of_week = my_date.isocalendar()
-#     print("Week #" + str(week_num))
-#     print("This week's games are: ")
-
-
-#     with open("lmpl.csv", "r") as f:
-#         reader = csv.reader(f)
-#         for row in reader:
-#             if row[0] == str(week_num):
-#                 print("\n" "Round " + row[1] + "," +
-#                     " " + "Game" + " " + row[2] + "," +
-#                     " " + row[3] + " " + "vs" + " " +
-#                     row[4] + " " + row[5] + "," +
-#                     " " + row[6] + "," + " " + row[7])
-#                 print("\n")
-#             if week_num in range(6, 43):
-#                 print("No Games this week.")
 
 # 3.  SELECT A ROUND TO VIEW
 
